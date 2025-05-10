b8b7fd9172017d4c930da0062ab3c9533565b3f3
Andrés Largo <andres@byyuto.com>
2025-05-10 01:21:30 -0500

    fix types

--- dist/src/components/Typography/Responsive.stories.d.ts
+++
@@ -1,4 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react/*';
-export declare const ResponsiveTypography: StoryObj;
-declare const meta: Meta;
-export default meta;
--- dist/src/components/Typography/Typography.stories.d.ts
+++
@@ -1,7 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react';
-declare const meta: Meta;
-export default meta;
-export declare const AllTypography: StoryObj;
-export declare const Headings: StoryObj;
-export declare const TextStyles: StoryObj;
-export declare const Links: StoryObj;
